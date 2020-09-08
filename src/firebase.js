13f82ea3905654a914df184fb2edf36cfc6ddae0
ketangupta34 <ketangupta34@gmail.com>
2020-09-09 02:22:11 +0530

    clean start

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATnNE_fjgITN5d8spun36D7gfbjsfbyT8",
+    authDomain: "clone-7473c.firebaseapp.com",
+    databaseURL: "https://clone-7473c.firebaseio.com",
+    projectId: "clone-7473c",
+    storageBucket: "clone-7473c.appspot.com",
+    messagingSenderId: "288709432508",
+    appId: "1:288709432508:web:8cea2e877799f51fe6ecb9",
+    measurementId: "G-75GN1E8G0R"
+  };
