7d9dde3a235312def169a8e26f6c90d805e220a1
ketangupta34 <ketangupta34@gmail.com>
2020-11-16 23:19:48 +0530

    side overlay working

--- src/components/TintBackground.js
+++ src/components/TintBackground.js
@@ -2,16 +2,6 @@ import React from "react";
 import "../stylesheet/tint.css";
 
 export default function TintBackground(props) {
-  // if (props.act) {
-  //   console.log("open");
-  //   document.querySelector(".tintBackgroundActive").style.display = "initial";
-  // } else {
-  //   window.setTimeout(() => {
-  //     console.log("close");
-  //     document.querySelector(".tintBackgroundClose").style.display = "none";
-  //   }, 200);
-  // }
-
   return (
     <div
       className={props.act ? "tintBackgroundActive" : "tintBackgroundClose"}
