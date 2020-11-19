a04af75b9344870c2a0750203efe13d32cc51eec
ketangupta34 <ketangupta34@gmail.com>
2020-11-20 01:40:39 +0530

    product 1 working

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import "../stylesheet/Home.css";
 import Product1 from "../components/Product1";
 import HomeCarausel from "../components/HomeCarausel";
 
+import oneplusImage from "../assets/OnePlus-7.jpg";
+
 export default function Home() {
   return (
     <div className="home">
@@ -12,26 +14,51 @@ export default function Home() {
       </div>
       <div className="products">
         <div className="productRow">
-          <Product1 />
-          <Product1 />
-          <Product1 />
+          <Product1
+            title="This is new Text 1"
+            price="Rs. 1100"
+            image={oneplusImage}
+            rating={5}
+          />
+          <Product1
+            title="This is new Text 2"
+            price="Rs. 1200"
+            image={oneplusImage}
+            rating={4}
+          />
+          <Product1
+            title="This is new Text 3"
+            price="Rs. 1300"
+            image={oneplusImage}
+            rating={3}
+          />
+          <Product1
+            title="This is new Text 4"
+            price="Rs. 1400"
+            image={oneplusImage}
+            rating={2}
+          />
         </div>
         <div className="productRow">
           <Product1 />
           <Product1 />
           <Product1 />
+          {/* <Product1 /> */}
+        </div>
+        <div className="productRow">
+          <Product1 />
+          <Product1 />
+          {/* <Product1 /> */}
+          {/* <Product1 /> */}
         </div>
         <div className="productRow">
           <Product1 />
           <Product1 />
           <Product1 />
+          {/* <Product1 /> */}
         </div>
         <div className="productRow">
           <Product1 />
-          <Product1 />
-          <Product1 />
-        </div>
-        <div className="productRow">
           <Product1 />
           <Product1 />
           <Product1 />
--- src/components/Product1.js
+++ src/components/Product1.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "../stylesheet/Product1.css";
 
-function Product1() {
+function Product1(props) {
   return (
     <div className="product1">
       <div className="productInfo">
-        <p>this is the description text</p>
-        <p>Rs 1999</p>
+        <p>{props.title}</p>
+        <p>{props.price}</p>
         <div className="starRating">
-          <p>🌟</p>
-          <p>🌟</p>
-          <p>🌟</p>
-          <p>🌟</p>
-          <p>🌟</p>
+          {console.log(props.rating)}
+          {Array(props.rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
       </div>
+      <img src={props.image} />
+      <button>Add to basket</button>
     </div>
   );
 }
